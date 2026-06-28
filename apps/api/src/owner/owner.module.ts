b33b7866d1c131f10c126122294d5f89872c3498
Nikita00NS <ns.nikita.official@gmail.com>
2026-06-28 21:48:55 +0300

    Update owner.module.ts

--- apps/api/src/owner/owner.module.ts
+++ apps/api/src/owner/owner.module.ts
@@ -2,5 +2,8 @@ import { Module } from '@nestjs/common';
 import { OwnerController } from './owner.controller';
 import { BotsModule } from '../bots/bots.module';
 
-@Module({ imports: [BotsModule], controllers: [OwnerController] })
+@Module({
+  imports: [BotsModule],
+  controllers: [OwnerController],
+})
 export class OwnerModule {}
