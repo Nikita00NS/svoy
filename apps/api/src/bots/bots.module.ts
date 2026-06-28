c6340a2a3e59bb3293081db13f720d59c19bb6cc
Nikita00NS <ns.nikita.official@gmail.com>
2026-06-28 18:38:29 +0300

    Delete apps directory

--- apps/api/src/bots/bots.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BotsController } from './bots.controller';
-import { BotsService } from './bots.service';
-import { TelegramModule } from '../telegram/telegram.module';
-
-@Module({ imports: [TelegramModule], controllers: [BotsController], providers: [BotsService] })
-export class BotsModule {}
--- apps/api/src/intakes/intakes.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IntakesService } from './intakes.service';
-import { IntakesController } from './intakes.controller';
-
-@Module({ providers: [IntakesService], controllers: [IntakesController], exports: [IntakesService] })
-export class IntakesModule {}
