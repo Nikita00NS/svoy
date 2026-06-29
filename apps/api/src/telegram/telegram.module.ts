b4361d6a8b0ae8bf86bd8aee78d5f15e9257d008
Nikita00NS <ns.nikita.official@gmail.com>
2026-06-29 19:13:26 +0300

    Update telegram.module.ts

--- apps/api/src/telegram/telegram.module.ts
+++ apps/api/src/telegram/telegram.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { OwnerController } from './owner.controller';
-import { BotsModule } from '../bots/bots.module';
+import { Module, forwardRef } from '@nestjs/common';
+import { TelegramController } from './telegram.controller';
+import { TelegramService } from './telegram.service';
+import { TelegramApiService } from './telegram-api.service';
+import { IntakesModule } from '../intakes/intakes.module';
+import { ModerationModule } from '../moderation/moderation.module';
+import { ContentModule } from '../content/content.module';
 
 @Module({
-  imports: [BotsModule],
-  controllers: [OwnerController],
+  imports: [
+    IntakesModule,
+    forwardRef(() => ModerationModule),
+    forwardRef(() => ContentModule),
+  ],
+  controllers: [TelegramController],
+  providers: [TelegramService, TelegramApiService],
+  exports: [TelegramApiService],
 })
-export class OwnerModule {}
+export class TelegramModule {}
