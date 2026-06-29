44674f8f161a6ee0f831b479f91df78082d8b377
Nikita00NS <ns.nikita.official@gmail.com>
2026-06-29 19:18:06 +0300

    Update owner.module.ts

--- apps/api/src/owner/owner.module.ts
+++ apps/api/src/owner/owner.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { OwnerController } from './owner.controller';
+import { OwnerService } from './owner.service';
 import { BotsModule } from '../bots/bots.module';
 
 @Module({
-  imports: [BotsModule],
+  imports: [forwardRef(() => BotsModule)],
   controllers: [OwnerController],
+  providers: [OwnerService],
 })
 export class OwnerModule {}
