58ba606bcc069e5a3cdbe82a769873fee5cf7b14
Nikita00NS <ns.nikita.official@gmail.com>
2026-07-11 00:23:03 +0300

    Delete apps directory

--- apps/api/src/queues/queues.module.ts
+++
@@ -1,5 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QueuesService } from './queues.service';
-
-@Module({ providers: [QueuesService], exports: [QueuesService] })
-export class QueuesModule {}
