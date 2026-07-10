7c97092bf94b6d95c18cb41ffa33c5aaa2415151
Nikita00NS <ns.nikita.official@gmail.com>
2026-07-11 00:05:55 +0300

    Delete apps directory

--- apps/api/src/owner/owner.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OwnerController } from './owner.controller';
-import { BotsModule } from '../bots/bots.module';
-
-@Module({
-  imports: [BotsModule],
-  controllers: [OwnerController],
-})
-export class OwnerModule {}
