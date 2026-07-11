e830a02a230132d8fb9f1fd1e9416a69e4f06e28
Nikita00NS <ns.nikita.official@gmail.com>
2026-07-11 22:13:30 +0300

    Delete apps directory

--- apps/api/src/admin-ui/admin-ui.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AdminUiController } from './admin-ui.controller';
-
-@Module({
-  controllers: [AdminUiController],
-})
-export class AdminUiModule {}
