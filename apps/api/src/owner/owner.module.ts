cb81ba84a45a3801a86ce195e6a8327736f8615e
Nikita00NS <ns.nikita.official@gmail.com>
2026-07-11 22:37:23 +0300

    Delete apps directory

--- apps/api/src/owner/owner.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, forwardRef } from '@nestjs/common';
-import { OwnerController } from './owner.controller';
-import { BotsModule } from '../bots/bots.module';
-
-@Module({
-  imports: [forwardRef(() => BotsModule)],
-  controllers: [OwnerController],
-})
-export class OwnerModule {}
